5ad1485189ed01a77c0bb4039144b971e4cf97d2
Andrew Jarombek <ajarombek95@gmail.com>
2018-11-26 20:26:29 -0500

    Fix

--- terraform-apply/terraform_apply.groovy
+++ terraform-apply/terraform_apply.groovy
@@ -15,7 +15,7 @@ pipelineJob("terraform-apply") {
                 git {
                     branch("master")
                     remote {
-                        credentials("ajarombek-github-ssh")
+                        credentials("865da7f9-6fc8-49f3-aa56-8febd149e72b")
                         github("AJarombek/global-jenkins-jobs", "ssh", "github.com")
                     }
                 }
