49018c1c2130ca7b4590ef0222775c779a04bee2
Andrew Jarombek <ajarombek95@gmail.com>
2019-05-19 12:23:12 -0400

    put k8s infra destroy in reverse order

--- kubernetes-prototype-infra/Jenkinsfile.groovy
+++ kubernetes-prototype-infra/Jenkinsfile.groovy
@@ -19,37 +19,17 @@ node("master") {
                   credentialsId: "865da7f9-6fc8-49f3-aa56-8febd149e72b",
                   userRemoteConfigs: [[url: "git@github.com:AJarombek/kubernetes-prototype.git"]]])
     }
-    stage("eks-cluster") {
-        ansiColor('css') {
-            tf(is_create, "infra/eks/cluster")
-        }
-    }
-    stage("bastion-key") {
-        dir("infra/bastion/key") {
-            def result = sh(
-                script: """
-                    aws ec2 describe-key-pairs --key-name eks-sandbox-bastion-key --query "KeyPairs" | jq length
-                """,
-                returnStdout: true
-            )
-            if (result == "0") {
-                ansiColor('css') {
-                    tf(is_create, "infra/bastion/key")
-                }
-            } else {
-                println "eks-sandbox-bastion-key already created."
-            }
-        }
-    }
-    stage("eks-nodes") {
-        ansiColor('css') {
-            tf(is_create, "infra/eks/nodes")
-        }
-    }
-    stage("bastion-host") {
-        ansiColor('css') {
-            tf(is_create, "infra/bastion/host")
-        }
+    if (is_create) {
+        eksClusterStage()
+        bastionKeyStage()
+        eksNodesStage()
+        bastionHostStage()
+    } else {
+        // When removing infrastructure, it should be destroyed in the reverse order of its creation
+        bastionHostStage()
+        eksNodesStage()
+        bastionKeyStage()
+        eksClusterStage()
     }
 }
 
@@ -64,5 +44,61 @@ def tf(boolean create, String path, String repository = 'git@github.com:AJarombe
         terraform.terraformApply(path, repository, true)
     } else {
         terraform.terraformDestroy(path, repository)
+    }
+}
+
+/**
+ * Pipeline stage that either creates or destroys the EKS Cluster
+ */
+def eksClusterStage() {
+    stage("eks-cluster") {
+        ansiColor('css') {
+            tf(is_create, "infra/eks/cluster")
+        }
+    }
+}
+
+/**
+ * Pipeline stage that either creates or destroys the SSH Key for the Bastion Host
+ */
+def bastionKeyStage() {
+    stage("bastion-key") {
+        dir("infra/bastion/key") {
+            def result = sh(
+                    script: """
+                    aws ec2 describe-key-pairs --key-name eks-sandbox-bastion-key --query "KeyPairs" | jq length
+                """,
+                    returnStdout: true
+            )
+            if (result == "0") {
+                ansiColor('css') {
+                    tf(is_create, "infra/bastion/key")
+                }
+            } else {
+                println "eks-sandbox-bastion-key already created."
+            }
+        }
+    }
+}
+
+/**
+ * Pipeline stage that either creates or destroys the EKS Cluster Nodes (EC2 Instances)
+ */
+def eksNodesStage() {
+    stage("eks-nodes") {
+        ansiColor('css') {
+            tf(is_create, "infra/eks/nodes")
+        }
+    }
+}
+
+/**
+ * Pipeline stage that either creates or destroys the EKS Cluster Bastion Host (EC2 Instance)
+ */
+def bastionHostStage() {
+    stage("bastion-host") {
+        ansiColor('css') {
+            tf(is_create, "infra/bastion/host")
+        }
     }
 }
