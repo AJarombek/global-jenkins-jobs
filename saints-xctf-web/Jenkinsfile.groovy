7b477bdc3913730a8217086e9a897f80cd65c2cb
Andrew Jarombek <ajarombek95@gmail.com>
2020-03-15 21:36:35 -0400

    saintsxctf web test outline

--- saints-xctf-web/Jenkinsfile.groovy
+++ saints-xctf-web/Jenkinsfile.groovy
@@ -4,6 +4,10 @@
  * @since 3/14/2020
  */
 
+// 15 West 64th Street, Begin setting up furniture, Too many IKEA trips
+
+@Library(['global-jenkins-library@master']) _
+
 final String PARAM_BRANCH = branch
 
 pipeline {
@@ -31,13 +35,79 @@ pipeline {
         stage("Checkout Repository") {
             steps {
                 script {
-                    checkout([
-                        $class: 'GitSCM',
-                        branches: [[name: "*/$PARAM_BRANCH"]],
-                        credentialsId: "ajarombek-github",
-                        userRemoteConfigs: [[url: "git@github.com:AJarombek/saints-xctf-web.git"]]
-                    ])
+                    git.basicClone('saints-xctf-web')
                 }
+            }
+        }
+        stage("Setup Project") {
+            steps {
+                script {
+                    sh '''
+                        set -x
+                        node --version
+                        npm --version
+                        
+                        npm install yarn -g
+                        yarn
+                    '''
+                }
+            }
+        }
+        stage("Execute Tests") {
+            steps {
+                script {
+                    try {
+                        def status = sh (
+                            script: """
+                                set +e
+                                set -x
+                                yarn run client:test >> test_results.log
+                                exit_status=\$?
+    
+                                cat test_results.log
+    
+                                exit \$exit_status
+                            """,
+                            returnStatus: true
+                        )
+
+                        if (status >= 1) {
+                            currentBuild.result = "UNSTABLE"
+                        }
+
+                    } catch (Exception ex) {
+                        echo "SaintsXCTF Web Application Testing Failed"
+                        currentBuild.result = "UNSTABLE"
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            script {
+                def bodyContent = ""
+                def testResultLog = ""
+
+                dir("test") {
+                    testResultLog = readFile "test_results.log"
+                }
+
+                testResultLog.split('\n').each {
+                    bodyContent += "<p style=\"font-family: Consolas,monaco,monospace\">$it</p>"
+                }
+
+                def bodyTitle = "SaintsXCTF Web Application Tests"
+                email.sendEmail(
+                    bodyTitle,
+                    bodyContent,
+                    env.JOB_NAME,
+                    currentBuild.result,
+                    env.BUILD_NUMBER,
+                    env.BUILD_URL
+                )
+
+                cleanWs()
             }
         }
     }
